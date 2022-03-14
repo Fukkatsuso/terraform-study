db8bb7c72606f76c197fabb395cbd85fa10b9049
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2022-03-14 16:14:03 +0900

    build infrastructure (GCP)

---
+++ learn-terraform-gcp/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("terraform-study-344105-af0b46f5563d.json")
+
+  project = "terraform-study-344105"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
