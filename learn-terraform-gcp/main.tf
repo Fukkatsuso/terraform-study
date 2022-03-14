9d07c1404137def04914b160cffcea37219974fa
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2022-03-14 18:05:11 +0900

    define input variables

--- learn-terraform-gcp/main.tf
+++ learn-terraform-gcp/main.tf
@@ -8,11 +8,11 @@ terraform {
 }
 
 provider "google" {
-  credentials = file("terraform-study-344105-af0b46f5563d.json")
+  credentials = file(var.credentials_file)
 
-  project = "terraform-study-344105"
-  region  = "us-central1"
-  zone    = "us-central1-c"
+  project = var.project
+  region  = var.region
+  zone    = var.zone
 }
 
 resource "google_compute_network" "vpc_network" {
